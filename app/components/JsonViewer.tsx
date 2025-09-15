f80bf47b790a6fdb24581abd5bdda8dbf09a8bcd
juliia2 <132770393+juliia2@users.noreply.github.com>
2025-09-15 15:17:45 -0400

    started app

--- app/components/JsonViewer.tsx
+++ app/components/JsonViewer.tsx
@@ -15,3 +15,6 @@ const jsonData = {};
 
 // nts: have a way to update the tile details when it opens the json. like, have a button:  
 // "Update Tile Details" that updates the tile details with the json data.
+
+
+// nts, 4:27 pm july 5, 2025: json content is now stored from index. work with that next. 
